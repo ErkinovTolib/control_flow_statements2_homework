2997a9c6d3d914cc4ec34127501b58c16ddd65d6
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-22 13:21:53 +0500

    if05

--- if05.py
+++ if05.py
@@ -6,4 +6,28 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    
+    a = n%10
+    n//=10   
+
+    b = n%10
+    n//=10
+
+    c = n%10
+    n//=10
+
+    d = n%10
+    n//=10
+
+    e = n%10
+    n//=10
+    if a>b and a>c and a>d and a>e:
+        answer = a
+    if b>a and b>c and b>d and b>e:
+        answer = b
+    if c>a and c>b and c>d and c>e:
+        answer = c
+    if d>a and d>b and d>c and d>e:
+        answer = d
+    return answer
+print(main(25689))
